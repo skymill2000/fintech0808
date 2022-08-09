f82c2ad76ae979391ee733321de57b900271c93a
유관우 <skymill2000@gmail.com>
2022-08-09 16:43:02 +0900

    StyleComponent 라이브러리 추가 StyleComponent sample page 추가

--- fintech/src/App.js
+++ fintech/src/App.js
@@ -1,13 +1,14 @@
 import "./App.css";
 import InputExample from "./components/InputExample";
 import ListComponent from "./components/ListComponent";
+import StyledComponent from "./components/StyledComponent";
 import Welcome from "./components/Welcome";
 
 function App() {
   return (
     <div className="App">
       <h1>안녕하세요</h1>
-      <ListComponent></ListComponent>
+      <StyledComponent></StyledComponent>
     </div>
   );
 }
