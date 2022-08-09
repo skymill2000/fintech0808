14ef51d73e3cddf35e75d685ddeda0b00ef7878a
유관우 <skymill2000@gmail.com>
2022-08-09 15:39:11 +0900

    Input change 이벤트 처리 button click 이벤트 바인딩

--- fintech/src/components/InputExample.js
+++ fintech/src/components/InputExample.js
@@ -1,10 +1,18 @@
 import React from "react";
 
 const InputExample = () => {
+  const handleChangeInput = (event) => {
+    const { value } = event.target;
+    console.log(value);
+  };
+
+  const handleClickButton = () => {
+    console.log("button click");
+  };
   return (
     <div>
-      <input></input>
-      <button>입력</button>
+      <input onChange={handleChangeInput}></input>
+      <button onClick={handleClickButton}>입력</button>
     </div>
   );
 };
