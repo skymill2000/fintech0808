4b2fd88e45d15761b102c6eb0e722c4af1c333b6
유관우 <skymill2000@gmail.com>
2022-08-09 11:24:44 +0900

    work01 : arrow function 선언하기

---
+++ basic_example/example02.js
@@ -0,0 +1,13 @@
+// 안씁니다
+function plus(p1, p2) {
+  return p1 + p2;
+}
+console.log(plus(3, 9));
+// 안쓸거에요
+
+const arrowPlus = (p1, p2) => {
+  return p1 + p2;
+};
+
+console.log(arrowPlus(3, 9));
+//work01 : Arrow Func으로 minus 기능, mul 곱하기 기능, div 나누기 기능을 구현
