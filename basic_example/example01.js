4c7e8ae0868e328f9de488b70dbd61748f6a03be
유관우 <skymill2000@gmail.com>
2022-08-09 11:11:59 +0900

    example01 ) 변수 선언 방법 var, let, const

---
+++ basic_example/example01.js
@@ -0,0 +1,13 @@
+// private String username = "홍길동"; <-- String 문자열
+// private int userage = 15 <-- 정수형
+
+var username = "홍길동";
+var username = "둘리";
+console.log(username);
+
+let age = 14;
+age = 20;
+console.log(age);
+
+const gender = "female";
+console.log(gender);
