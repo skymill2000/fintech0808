1cd319e9554da200a937f7a8032ba843608a01a2
유관우 <skymill2000@gmail.com>
2022-08-09 11:34:09 +0900

    work01) 사칙연산 완성

--- basic_example/example02.js
+++ basic_example/example02.js
@@ -11,3 +11,20 @@ const arrowPlus = (p1, p2) => {
 
 console.log(arrowPlus(3, 9));
 //work01 : Arrow Func으로 minus 기능, mul 곱하기 기능, div 나누기 기능을 구현
+
+const minus = (p1, p2) => {
+  return p1 - p2;
+};
+
+const mul = (p1, p2) => {
+  return p1 * p2;
+};
+
+const div = (p1, p2) => {
+  return p1 / p2;
+};
+
+console.log(plus(5, 2));
+console.log(minus(5, 2));
+console.log(mul(5, 2));
+console.log(div(5, 2));
