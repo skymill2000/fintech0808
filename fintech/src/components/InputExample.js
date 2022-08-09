6c1b9b23782792ee05b5174da5396b00d7b57727
유관우 <skymill2000@gmail.com>
2022-08-09 15:48:07 +0900

    스테이트 처리 전

--- fintech/src/components/InputExample.js
+++ fintech/src/components/InputExample.js
@@ -1,16 +1,22 @@
 import React from "react";
 
 const InputExample = () => {
+  let userInput = "입력 받기전";
+
   const handleChangeInput = (event) => {
     const { value } = event.target;
     console.log(value);
+    userInput = value;
+    console.log(userInput);
   };
 
   const handleClickButton = () => {
     console.log("button click");
   };
+
   return (
     <div>
+      <p>{userInput}</p>
       <input onChange={handleChangeInput}></input>
       <button onClick={handleClickButton}>입력</button>
     </div>
