8d6854c8d768752f4fc6eb277900f580a58a8bb4
유관우 <skymill2000@gmail.com>
2022-08-10 13:41:35 +0900

    헤더 컴포넌트 추가

--- fintech/src/pages/NewsPage.js
+++ fintech/src/pages/NewsPage.js
@@ -1,7 +1,12 @@
 import React from "react";
+import AppHeader from "../components/common/AppHeader";
 
 const NewsPage = () => {
-  return <div>NewsPage</div>;
+  return (
+    <div>
+      <AppHeader title={"뉴스 검색"}></AppHeader>
+    </div>
+  );
 };
 
 export default NewsPage;
