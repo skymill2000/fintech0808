fd9f51c65fd07a046169439ff5d905bb10f81fd9
유관우 <skymill2000@gmail.com>
2022-08-10 14:43:48 +0900

    #work3 뉴스 검색 api 를 활용하여 뉴스 검색 어플리케이션 작성

--- fintech/src/components/news/NewsList.js
+++ fintech/src/components/news/NewsList.js
@@ -1,7 +1,13 @@
 import React from "react";
 
-const NewsList = () => {
-  return <div>NewsList</div>;
+const NewsList = ({ newsList }) => {
+  return (
+    <div>
+      {newsList.articles.map((article) => {
+        return <p>{article.title}</p>;
+      })}
+    </div>
+  );
 };
 
 export default NewsList;
