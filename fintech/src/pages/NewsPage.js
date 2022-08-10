90bc5def7ac6c809f7eecb579844c65d580e8756
유관우 <skymill2000@gmail.com>
2022-08-10 13:50:08 +0900

    #work3 뉴스 검색 어플리케이션 작성하기

--- fintech/src/pages/NewsPage.js
+++ fintech/src/pages/NewsPage.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import AppHeader from "../components/common/AppHeader";
+import NewsList from "../components/news/NewsList";
+import NewsSearch from "../components/news/NewsSearch";
 
 const NewsPage = () => {
+  const [searchInput, setSearchInput] = useState("");
+  //1. searchInput 데이터를 newsSearch 컴포넌트로 부터 받아 오기
+  const [searchList, setSearchList] = useState([]);
+  //2. axios를 통해서 데이터를 요청하여 내역 searchList 채우기
+
   return (
     <div>
       <AppHeader title={"뉴스 검색"}></AppHeader>
+      <NewsSearch></NewsSearch>
+      <NewsList></NewsList>
     </div>
   );
 };
