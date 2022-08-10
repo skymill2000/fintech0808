bd63e4c552b51c17bde5238e9df985dc0ca5511e
유관우 <skymill2000@gmail.com>
2022-08-10 13:33:13 +0900

    뉴스 검색 어플리케이션 작성 준비

--- fintech/src/components/AxiosTest.js
+++ fintech/src/components/AxiosTest.js
@@ -4,10 +4,14 @@ import axios from "axios";
 const AxiosTest = () => {
   const handleClick = () => {
     let data = "아직 아무것도 없습니다.";
-    axios.get("https://jsonplaceholder.typicode.com/posts").then((response) => {
-      data = response.data;
-      console.log(data);
-    });
+    axios
+      .get(
+        "https://newsapi.org/v2/everything?q=삼성&from=2022-07-10&sortBy=publishedAt&apiKey=78bc6ddd8cdb48ceac76f5f9b9dfc4c5&language=ko"
+      )
+      .then((response) => {
+        data = response.data;
+        console.log(data);
+      });
   };
 
   return (
