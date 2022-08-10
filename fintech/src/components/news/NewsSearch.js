267a23b29a0834c0346cbdcc1470a0b71d08f543
유관우 <skymill2000@gmail.com>
2022-08-10 14:18:10 +0900

    work3  뉴스 검색 검색 입력 로직 완성

--- fintech/src/components/news/NewsSearch.js
+++ fintech/src/components/news/NewsSearch.js
@@ -1,9 +1,10 @@
 import React from "react";
 
-const NewsSearch = ({ handleChange }) => {
+const NewsSearch = ({ handleChange, handleClick }) => {
   return (
     <div>
       <input onChange={handleChange}></input>
+      <button onClick={handleClick}>검색</button>
     </div>
   );
 };
--- fintech/src/pages/NewsPage.js
+++ fintech/src/pages/NewsPage.js
@@ -9,15 +9,26 @@ const NewsPage = () => {
   const [searchList, setSearchList] = useState([]);
   //2. axios를 통해서 데이터를 요청하여 내역 searchList 채우기
 
-  const handleChange = () => {
-    console.log("change Event");
+  const handleChange = (event) => {
+    const { value } = event.target;
+    console.log(value);
+    setSearchInput(value);
+  };
+
+  const handleClick = () => {
+    console.log(searchInput);
+    //search input 바탕으로 뉴스 검색 api Request 요청
+    //axios 를 이용해서 데이터 리스폰스를 받아온 다음에 setSearchList
   };
 
   return (
     <div>
       <AppHeader title={"뉴스 검색"}></AppHeader>
-      <NewsSearch handleChange={handleChange}></NewsSearch>
-      <NewsList></NewsList>
+      <NewsSearch
+        handleChange={handleChange}
+        handleClick={handleClick}
+      ></NewsSearch>
+      <NewsList newsList={searchList}></NewsList>
     </div>
   );
 };
