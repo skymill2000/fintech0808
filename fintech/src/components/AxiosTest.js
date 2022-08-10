ff9374d132c33e73a9be018eb4044094d2ec4c05
유관우 <skymill2000@gmail.com>
2022-08-10 11:03:07 +0900

    axios 테스트 코드 추가

--- fintech/src/components/AxiosTest.js
+++ fintech/src/components/AxiosTest.js
@@ -1,7 +1,18 @@
 import React from "react";
+import axios from "axios";
 
 const AxiosTest = () => {
-  return <div>AxiosTest</div>;
+  const handleClick = () => {
+    axios.get("https://naver.com").then((data) => {
+      console.log(data);
+    });
+  };
+
+  return (
+    <div>
+      <button onClick={handleClick}>요청 보내기</button>
+    </div>
+  );
 };
 
 export default AxiosTest;
