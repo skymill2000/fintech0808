38e7e30c798dda7ff973e5b869cf6c4e3e7d0578
유관우 <skymill2000@gmail.com>
2022-08-11 10:54:18 +0900

    쿼리스트링 파싱 라이브러리 추가 npm install query-string

--- fintech/src/pages/AuthResultPage.js
+++ fintech/src/pages/AuthResultPage.js
@@ -1,6 +1,10 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
+import queryString from "query-string";
 
 const AuthResultPage = () => {
+  const parsedCode = queryString.parse(useLocation().search);
+  console.log("사용자 인증 코드 : ", parsedCode.code);
   return <div>AuthResultPage</div>;
 };
 
