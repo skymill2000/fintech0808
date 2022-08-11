0d0688a467c87140e01eacef714f1ac093c86732
유관우 <skymill2000@gmail.com>
2022-08-11 11:30:16 +0900

    url-encoded 데이터로 전송 데이터 형 변환

--- fintech/src/pages/AuthResultPage.js
+++ fintech/src/pages/AuthResultPage.js
@@ -5,22 +5,30 @@ import AppHeader from "../components/common/AppHeader";
 import axios from "axios";
 
 const AuthResultPage = () => {
-  const parsedCode = queryString.parse(useLocation().search);
+  const { code } = queryString.parse(useLocation().search);
   const [accessToken, setAccessToken] = useState("토큰을 받기 전");
   const [userSeqNo, setUserSeqNo] = useState("사용자 고유 식별 번호 받기 전");
 
   const handleClick = () => {
     console.log("토큰 요청");
+
+    const data = {
+      code: code,
+      client_id: "565a6aff-7182-4e43-8965-525a1c13a3d9",
+      client_secret: "c68472dd-b377-48a0-a0c3-0a28b978ba28",
+      redirect_uri: "http://localhost:3000/authResult",
+      grant_type: "authorization_code",
+    };
+
+    const parsedUrlEncodedData = queryString.stringify(data);
+
     const option = {
       method: "POST",
       url: "https://testapi.openbanking.or.kr/oauth/2.0/token",
       headers: {
         "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
       },
-      data: {
-        code: ".....",
-        "....": ".....",
-      },
+      data: parsedUrlEncodedData,
     };
     axios(option).then((response) => {
       console.log(response);
@@ -30,7 +38,7 @@ const AuthResultPage = () => {
   return (
     <div>
       <AppHeader title={"인증결과"}></AppHeader>
-      <p>인증 코드 : {parsedCode.code}</p>
+      <p>인증 코드 : {code}</p>
       <p>accessToken : {accessToken}</p>
       <p>userSeqNo : {userSeqNo}</p>
       <button onClick={handleClick}>AccessToken 요청하기</button>
