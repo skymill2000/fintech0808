61832104d1f0a93c8cf5eaef41d1d9a0946e404f
유관우 <skymill2000@gmail.com>
2022-08-11 11:11:31 +0900

    토큰 및 코드 데이터 확인용 소스 추가 인증 요청 버튼 추가

--- fintech/src/pages/AuthResultPage.js
+++ fintech/src/pages/AuthResultPage.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { useLocation } from "react-router-dom";
 import queryString from "query-string";
+import AppHeader from "../components/common/AppHeader";
 
 const AuthResultPage = () => {
   const parsedCode = queryString.parse(useLocation().search);
-  console.log("사용자 인증 코드 : ", parsedCode.code);
-  return <div>AuthResultPage</div>;
+  const [accessToken, setAccessToken] = useState("토큰을 받기 전");
+  const [userSeqNo, setUserSeqNo] = useState("사용자 고유 식별 번호 받기 전");
+
+  const handleClick = () => {
+    console.log("토큰 요청");
+  };
+
+  return (
+    <div>
+      <AppHeader title={"인증결과"}></AppHeader>
+      <p>인증 코드 : {parsedCode.code}</p>
+      <p>accessToken : {accessToken}</p>
+      <p>userSeqNo : {userSeqNo}</p>
+      <button onClick={handleClick}>AccessToken 요청하기</button>
+    </div>
+  );
 };
 
 export default AuthResultPage;
