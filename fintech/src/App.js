5bd3e250ca3b7e59251cbf35b7a9573cdcd0fbfa
유관우 <skymill2000@gmail.com>
2022-08-11 10:34:50 +0900

    인증 버튼 생성 및 새텝에 인증 페이지 이동

--- fintech/src/App.js
+++ fintech/src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import IndexPage from "./pages/IndexPage";
 import NewsPage from "./pages/NewsPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<IndexPage />}></Route>
         <Route path="/news" element={<NewsPage />}></Route>
       </Routes>
     </BrowserRouter>
