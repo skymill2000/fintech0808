9bf289cfa8d95bd97cbdeae05e553da4a38da1f3
유관우 <skymill2000@gmail.com>
2022-08-12 13:37:40 +0900

    결제 처리를 위한 프론트 모달 페이지 작업 완료

---
+++ fintech/src/components/withdraw/ModalCard.js
@@ -0,0 +1,61 @@
+import axios from "axios";
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const ModalCardBlock = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 0.5rem;
+  padding: 20px;
+  border: 1px #112211 solid;
+`;
+const CardTitle = styled.div`
+  font-size: 1rem;
+  color: black;
+`;
+const FintechUseNo = styled.div`
+  font-size: 0.7rem;
+  margin-bottom: 30px;
+`;
+
+const WithDrawButton = styled.button`
+  border: none;
+  padding: 0.3rem;
+  background: #2aa450;
+  color: white;
+  margin-top: 0.3rem;
+`;
+
+const ModalCard = ({ bankName, fintechUseNo, tofintechno }) => {
+  const [amount, setamount] = useState("");
+
+  const genTransId = () => {
+    let countnum = Math.floor(Math.random() * 1000000000) + 1;
+    let transId = "T991599190U" + countnum; //이용기과번호 본인것 입력
+    return transId;
+  };
+
+  const handlePayButtonClick = () => {
+    //출금 이체 발생시키기
+    //data params json
+  };
+
+  const deposit = () => {};
+
+  const handleChange = (e) => {
+    const { value } = e.target;
+    setamount(value);
+  };
+
+  return (
+    <ModalCardBlock>
+      <CardTitle>{bankName}</CardTitle>
+      <FintechUseNo>{fintechUseNo}</FintechUseNo>
+      <p>{tofintechno}로 돈을 보냅니다.</p>
+      <input onChange={handleChange}></input>
+      <WithDrawButton onClick={handlePayButtonClick}>결제하기</WithDrawButton>
+    </ModalCardBlock>
+  );
+};
+
+export default ModalCard;
